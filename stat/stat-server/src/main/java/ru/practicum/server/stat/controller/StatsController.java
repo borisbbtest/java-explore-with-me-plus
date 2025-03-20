ef7fafd1c5382dd1055f9032d6ce7831a73dd2bf
Boris <boris.z.077@gmail.com>
2025-03-21 02:26:52 +0300

    added 2F Table and logs

--- stat/stat-server/src/main/java/ru/practicum/server/stat/controller/StatsController.java
+++ stat/stat-server/src/main/java/ru/practicum/server/stat/controller/StatsController.java
@@ -1,7 +1,6 @@
 package ru.practicum.server.stat.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.stat.dto.EndpointHitDto;
