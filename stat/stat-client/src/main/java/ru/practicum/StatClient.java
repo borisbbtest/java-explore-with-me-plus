630d3c4e3e447de14a2d974a52468151446fd265
KuznetsovOS <KuznetsovOS@kadastr.ru>
2025-03-20 23:22:37 +0300

    feat: client

---
+++ stat/stat-client/src/main/java/ru/practicum/StatClient.java
@@ -0,0 +1,70 @@
+package ru.practicum;
+
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.lang.Nullable;
+import org.springframework.web.client.RestClient;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+
+public class StatClient {
+    protected final RestClient restClient;
+    private static final String HIT_ENDPOINT = "/hit";
+    private static final String STATS_ENDPOINT = "/stats";
+
+    protected StatClient(String baseUrl) {
+        this.restClient = RestClient.builder()
+                .baseUrl(baseUrl)
+                .defaultHeaders(headers -> {
+                    headers.setContentType(MediaType.APPLICATION_JSON);
+                })
+                .defaultStatusHandler(
+                        status -> status.is4xxClientError() || status.is5xxServerError(),
+                        (request, response) -> {
+                            throw new RestClientException("HTTP error: " + response.getStatusText());
+                        })
+                .build();
+    }
+
+    public void saveStatEvent(EndpointHitDto endpointHitDto) {
+        restClient.post()
+                .uri(HIT_ENDPOINT)
+                .body(endpointHitDto)
+                .retrieve()
+                .toBodilessEntity();
+    }
+
+    public ResponseEntity<List<ViewStatsDto>> getStats(String start,
+                                                    String end,
+                                                    @Nullable List<String> uris,
+                                                    boolean unique) {
+        String uri = buildStatsUri(start, end, uris, unique);
+
+        return restClient.get()
+                .uri(uri)
+                .retrieve()
+                .toEntity(new ParameterizedTypeReference<>() {});
+    }
+
+    private String buildStatsUri(String start, String end, @Nullable List<String> uris, boolean unique) {
+        UriComponentsBuilder builder = UriComponentsBuilder.fromPath(STATS_ENDPOINT)
+                .queryParam("start", encodeValue(start))
+                .queryParam("end", encodeValue(end))
+                .queryParam("unique", unique);
+
+        if (uris != null && !uris.isEmpty()) {
+            builder.queryParam("uris", String.join(",", uris));
+        }
+
+        return builder.build().toUriString();
+    }
+
+    private String encodeValue(String value) {
+        return URLEncoder.encode(value, StandardCharsets.UTF_8);
+    }
+}
