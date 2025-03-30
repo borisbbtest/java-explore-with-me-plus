ec5ff6739be7955f35f1bb26fdcfcdd597fe4d0d
Ilya Kravchenko <kravchi0803@gmail.com>
2025-03-30 16:49:31 +0300

    feat: mapper

--- main-service/src/main/java/ru/practicum/compilation/dto/CompilationDto.java
+++ main-service/src/main/java/ru/practicum/compilation/dto/CompilationDto.java
@@ -1,13 +1,16 @@
 package ru.practicum.compilation.dto;
 
+import lombok.Builder;
 import lombok.Data;
+import ru.practicum.event.dto.EventShortDto;
 
 import java.util.List;
 
 @Data
+@Builder
 public class CompilationDto {
     private Long id;
     private String title;
     private boolean pinned;
-    /*private List<EventShortDto> events;*/
+    private List<EventShortDto> events;
 }
