3d84b43a972c5741edafe82d3bf2abaf7a844f40
Ilya Kravchenko <kravchi0803@gmail.com>
2025-03-30 17:41:04 +0300

    feat: service impl and repository

--- main-service/src/main/java/ru/practicum/compilation/dto/UpdateCompilationRequest.java
+++ main-service/src/main/java/ru/practicum/compilation/dto/UpdateCompilationRequest.java
@@ -8,7 +8,7 @@ import java.util.List;
 @Data
 public class UpdateCompilationRequest {
     private List<Long> events;
-    private boolean pinned;
+    private Boolean pinned;
     @Size(min = 1, max = 50, message = "Заголовок подборки должен быть от 1 до 50 символов")
     private String title;
 }
