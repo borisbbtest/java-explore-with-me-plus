b0f041ccb1b0a05df7ce1fb34a9e43f143844a62
KuznetsovOS <KuznetsovOS@kadastr.ru>
2025-03-31 10:20:12 +0300

    Update RequestMapper.java

--- main-service/src/main/java/ru/practicum/request/mapper/RequestMapper.java
+++ main-service/src/main/java/ru/practicum/request/mapper/RequestMapper.java
@@ -1,10 +1,8 @@
 package ru.practicum.request.mapper;
 
-import org.springframework.stereotype.Component;
 import ru.practicum.request.dto.ParticipationRequestDto;
 import ru.practicum.request.model.Request;
 
-@Component
 public class RequestMapper {
 
     public static ParticipationRequestDto toRequestDto(Request request) {
